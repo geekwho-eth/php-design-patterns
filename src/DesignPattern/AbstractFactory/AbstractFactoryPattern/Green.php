a503893deba1e357f350de1434a508a181294d82
GeekWho <caijiamx@gmail.com>
2019-12-08 10:22:50 +0800

    feat: 新增抽象工厂模式以及测试用例

---
+++ src/DesignPattern/AbstractFactory/AbstractFactoryPattern/Green.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DesignPattern\AbstractFactory\AbstractFactoryPattern;
+
+class Green implements IColor
+{
+    public function fill()
+    {
+        echo "Inside Green::fill() method.";
+    }
+}
