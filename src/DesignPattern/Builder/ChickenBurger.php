75419b7ead29cbdf564b0dcf99ba4144c7f7b8fd
GeekWho <geekwho_eth@outlook.com>
2020-08-02 19:05:07 +0800

    feat: 新增建造者模式以及测试用例

---
+++ src/DesignPattern/Builder/ChickenBurger.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DesignPattern\Builder;
+
+/**
+ * 鸡肉汉堡
+ */
+class ChickenBurger extends Burger
+{
+    public function name()
+    {
+        return "Chicken Burger";
+    }
+
+    public function price()
+    {
+        return 50;
+    }
+}
